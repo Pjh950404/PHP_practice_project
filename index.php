966c1191d1c42ce919e99d8fec590b9a9b9c653c
m_yg@naver.com <m_yg@naver.com>
2017-05-30 21:31:45 +0900

    header add

--- index.php
+++ index.php
@@ -1,26 +1,94 @@
 <!DOCTYPE html>
 <head>
   <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
-
-  <title> 이민영의 포트폴리오 페이지 </title>
-  <img src=/imgfolder/index.jpeg alt="나의 페이지">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <title>  leemy.me - PortfolioWebSite</title>
+</div>
 </head>
 
 <body>
 
-  <ul>
-    <li><a href="#home">홈화면</a></li>
-    <li><a href="#news">이력사항</a></li>
-    <li class="dropdown">
-      <a href="#dropdown" class="dropbtn">상담</a>
-      <div class="dropdown-content">
-        <a href="#">포트폴리오</a>
-        <a href="#">게시판</a>
-        <a href="#">속풀이</a>
+  <!-- 상단 네비게이션 부분 -->
+  <div class="w3-top">
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card-2">
+      <a href="/index.php" class="w3-bar-item w3-button"><b>Lee</b> PortfolioWebSite</a>
+
+      <div class="w3-right w3-hide-small">
+        <a href="#projects" class="w3-bar-item w3-button">이력사항</a>
+        <a href="#about" class="w3-bar-item w3-button">게시판</a>
+        <a href="/login.php" class="w3-bar-item w3-button">로그인</a>
       </div>
-    </li>
-    <li><a href="#news">게임</a></li>
-  </ul>
+    </div>
+  </div>
+  <div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+
+  <!-- 사진부분 -->
+  <!-- Header -->
+<header id="portfolio">
+  <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+  <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+  <div class="w3-container">
+  <h1><b>My Portfolio</b></h1>
+  <div class="w3-section w3-bottombar w3-padding-16">
+    <span class="w3-margin-right">Filter:</span>
+    <button class="w3-button w3-black">ALL</button>
+    <button class="w3-button w3-white"><i class="fa fa-diamond w3-margin-right"></i>Design</button>
+    <button class="w3-button w3-white w3-hide-small"><i class="fa fa-photo w3-margin-right"></i>Photos</button>
+    <button class="w3-button w3-white w3-hide-small"><i class="fa fa-map-pin w3-margin-right"></i>Art</button>
+  </div>
+  </div>
+</header>
+
+<!-- First Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="/w3images/mountains.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="/w3images/lights.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="/w3images/nature.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="/w3images/p1.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="/w3images/p2.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="/w3images/p3.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Lorem Ipsum</b></p>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
